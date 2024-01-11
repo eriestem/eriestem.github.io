ca4b43e2c7022e1e734dfd60ae8137e198467c7c
Edward Shim <30818213+eriestem@users.noreply.github.com>
2024-01-11 00:00:00 -0500

    Add files via upload

---
+++ JS/typed.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed1 = new Typed(".auto-type", {
+        strings: ["Who Am I?"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+
+    var typed2 = new Typed(".auto-type1", {
+        strings: ["Places I Have Lived In"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+
+    var typed3 = new Typed(".auto-type2", {
+        strings: ["Get In Touch!"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+});
