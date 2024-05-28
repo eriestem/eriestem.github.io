fd5bc1d83d9490f3243abc2743a239a8eac71024
eriestem <ekim17318@gmail.com>
2024-05-28 15:24:39 -0400

    update0

--- JS/typed.js
+++ JS/typed.js
@@ -5,16 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
         backSpeed: 150,
         loop: true
     });
+});
 
+document.addEventListener('DOMContentLoaded', function () {
     var typed2 = new Typed(".auto-type1", {
         strings: ["Places I Have Lived In"],
         typeSpeed: 150,
         backSpeed: 150,
         loop: true
     });
+});
 
+document.addEventListener('DOMContentLoaded', function () {
     var typed3 = new Typed(".auto-type2", {
-        strings: ["Get In Touch!"],
+        strings: ["Education and Experiences"],
+        typeSpeed: 150,
+        backSpeed: 150,
+        loop: true
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var typed3 = new Typed(".auto-type3", {
+        strings: ["Coursework"],
         typeSpeed: 150,
         backSpeed: 150,
         loop: true
